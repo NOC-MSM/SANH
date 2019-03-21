2cff67227c6896378ab660d6d314b44c7f262f0b
jpolton <jelt@noc.ac.uk>
2019-03-21 21:57:28 +0000

    Update make_xios.sh
    Updated ARCH path as $GITCLONE subdir

--- SCRIPTS/make_xios.sh
+++ SCRIPTS/make_xios.sh
@@ -3,7 +3,7 @@ cd $WORK
 svn co -r1242 http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/trunk $XIOS_DIR
 cd $XIOS_DIR
 
-cp $ARCH1/arch-XC30_ARCHER* arch/.
+cp $GITCLONE/ARCH/arch-XC30_ARCHER* arch/.
 
 ./make_xios --full --prod --arch XC30_ARCHER --netcdf_lib netcdf4_par
 
