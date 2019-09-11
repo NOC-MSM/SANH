de5ec1105f05957599a04710efde838a0865b9e5
jenjar93 <54066506+jenjar93@users.noreply.github.com>
2019-09-11 22:51:52 +0530

    Update make_domain_cfg.sh
    Added path to the namelist_cfg file I used in BoBEAS

--- SCRIPTS/make_domain_cfg.sh
+++ SCRIPTS/make_domain_cfg.sh
@@ -1,6 +1,6 @@
 cp $DOMAIN/coordinates.nc     $TDIR/DOMAINcfg/.
 cp $DOMAIN/bathy_meter.nc     $TDIR/DOMAINcfg/.
-cp $GFILE/DOMAIN/namelist_cfg  $TDIR/DOMAINcfg/.
+cp /work/n01/n01/jenjar93/BoBEAS/DOMAIN/namelist_cfg  $TDIR/DOMAINcfg/.
 cp $GFILE/DOMAIN/rs.pbs        $TDIR/DOMAINcfg/.
 #cp $WDIR/make_paths.sh        $TDIR/DOMAINcfg/.
 cp $WDIR/../SANH/SCRIPTS/make_paths.sh        $TDIR/DOMAINcfg/.
