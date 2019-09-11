b5b0bb0851da1afd6615da2ae90c0b5cda91aa69
jenjar93 <54066506+jenjar93@users.noreply.github.com>
2019-09-11 22:06:33 +0530

    Update make_domain_cfg.sh
    Changed BoBEAS -> SANH

--- SCRIPTS/make_domain_cfg.sh
+++ SCRIPTS/make_domain_cfg.sh
@@ -3,7 +3,7 @@ cp $DOMAIN/bathy_meter.nc     $TDIR/DOMAINcfg/.
 cp $GFILE/DOMAIN/namelist_cfg  $TDIR/DOMAINcfg/.
 cp $GFILE/DOMAIN/rs.pbs        $TDIR/DOMAINcfg/.
 #cp $WDIR/make_paths.sh        $TDIR/DOMAINcfg/.
-cp $WDIR/../BoBEAS/SCRIPTS/make_paths.sh        $TDIR/DOMAINcfg/.
+cp $WDIR/../SANH/SCRIPTS/make_paths.sh        $TDIR/DOMAINcfg/.
 
 cd $TDIR/DOMAINcfg
 qsub  rs.pbs
